4f7b8f1d48454d859959789acf06380e94332abb
Xifei Wang <xifeiw@uw.edu>
2018-05-25 17:02:58 -0700

    UI Xifei

--- ui.R
+++ ui.R
@@ -9,22 +9,46 @@ ui <- fluidPage(
       "Introduction"
       
     ),
-    
     # the second page
     tabPanel(
-      "Birthday by State and Team",
+      "Birth place by State and Team",
       sidebarLayout(
         sidebarPanel(
           # select widget by states
-          textInput(
-            inputId = "state",
-            label = "State Selection",
+          selectInput(
+            inputId = "team",
+            label = "Which team do u want to view?",
             choices = list(
-              "Illinois" = "IL",
-              "Indiana" = "IN",
-              "Michigan" = "MI",
-              "Ohio" = "OH",
-              "Wisconsin" = "WI"
+              "Hawks" = "Hawks",
+              "Celtics" = "Celtics",
+              "Nets" = "Nets",
+              "Hornets" = "Hornets",
+              "Bulls" = "Bulls",
+              "Cavaliers" = "Cavaliers",
+              "Mavericks" = "Mavericks",
+              "Nuggets" = "Nuggets",
+              "Pistons" = "Pistons",
+              "Warriors" = "Warrirors",
+              "Rockets" = "Rockets",
+              "Pacers" = "Pacers",
+              "Clippers" = "Clippers",
+              "Lakers" = "Lakers",
+              "Grizzlies" = "Grizzlies",
+              "Heat" = "Heat",
+              "Bucks" = "Bucks",
+              "Timberwolves" = "Timberwolves",
+              "Pelicans" = "Pelicans",
+              "Knicks" = "Knicks",
+              "Thunder" = "Thunder",
+              "Magic" = "Magic",
+              "76ers" = "76ers",
+              "Suns" = "Suns",
+              "Blazers" = "Blazers",
+              "Kings" = "Kings",
+              "Spurs"= "Spurs",
+              "Raptors" = "Raptors",
+              "Jazz" = "Jazz",
+              "Wizards" = "Wizards"
             )
           )
         ),
@@ -34,10 +58,11 @@ ui <- fluidPage(
       )
     ),
     tabPanel(
-      
-      
+      "fhf"
+    
     ),
     tabPanel(
+      "a"
       
     )
   )
